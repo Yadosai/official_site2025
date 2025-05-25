b869161fbed958200575a8490b9ebf224998f608
mituki099 <suumii.0999@icloud.com>
2025-05-25 16:51:11 +0900

    kikakuupdate

--- front/components/top/index.vue
+++ front/components/top/index.vue
@@ -12,6 +12,7 @@
         <p class="updateInfoTitle">更新情報</p>
         <div class="updateInfoContent">
             <updataInfoItem updateDate="5/20" contentText="協賛団体情報を今年度の情報に更新しました！" />
+            <updataInfoItem updateDate="5/25" contentText="企画情報を公開しました！" />
         </div>
     </div>
     <div id="LinkButtons">
